5e74f5f9efe0c76f04e402a9dfcc3b5f531ab50a
notGabo <ga.soto@duocuc.cl>
2023-09-05 01:30:40 -0300

    mensaje + insultos randoms

--- main.js
+++ main.js
@@ -4,25 +4,40 @@ const server_id_env = process.env.SERVER_ID;
 const channel_id_env = process.env.CHANNEL_ID;
 const discord = require('discord.js');
 
-//const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES] });
-const client = new discord.Client({ intents: 769});
+const client = new discord.Client({ intents: 769 });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+const insultos = [
+    'waton',
+    'autista',
+    'racista culiao',
+    'maricon',
+    'puto',
+    'cagon',
+    'cagao',
+    'cagón',
+]
+
+const mensajes = {
+    'League of Legends En cola': 'dejate de jugar lol',
+    'VALORANT': 'dejate de jugar valorant',
+    'Counter-Strike: Global Offensive': 'dejate de jugar cs',
+}
+
 client.on('presenceUpdate', (oldPresence, newPresence) => {
     const user = newPresence.member.user;
-    const game = newPresence.activities.find(activity => activity.name === 'League of Legends');
-    const serverId = server_id_env; // Replace with the ID of the server you want to target
-    const channelId = channel_id_env; // Replace with the ID of the channel you want to send the message to
-    // get the id of the user
-
-
+    const game = newPresence.activities.find(activity => mensajes[activity.name]);
+    const serverId = server_id_env; 
+    const channelId = channel_id_env; 
     if (game && newPresence.guild.id === serverId) {
       const channel = client.channels.cache.get(channelId); 
-      channel.send(`<@${user.id}> dejate de jugar lol xd`);
-      console.log(`@${user.username} dejate de jugar lol xd`)
+      maxInsultos = insultos.length
+      channel.send(`<@${user.id}> ${mensajes[game.name]} ${insultos[ Math.floor(Math.random() * maxInsultos)]}`);
+      console.log(`@${user.username} ${mensajes[game.name]} ${insultos[ Math.floor(Math.random() * maxInsultos)]}`)
     }
 });
 
